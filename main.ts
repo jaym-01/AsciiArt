dd3f56d835e9706c3c9367c17d349fd6998f121f
jay <jay_mistry@live.co.uk>
2025-02-27 22:49:53 +0000

    latest

--- main.ts
+++ main.ts
@@ -3,14 +3,15 @@ import { Image } from 'image-js';
 const chars = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'.";
 
 async function main() {
-    const image = (await Image.load('pic2.jpg')).grey();
+    const image = (await Image.load('dog8.png')).grey();
     const asciiData = image.data.map((val)=>{
-        return chars[Math.floor(val/255 * chars.length)];
+        return chars[Math.floor((val/255) * chars.length)];
     });
+    // console.log(asciiData.filter(val=>val!==69))
     let buffer = "";
     asciiData.forEach((val, i)=>{
         buffer += val;
-        if(i % image.width === 0) buffer += '\n';
+        if((i + 1) % image.width === 0) buffer += '\n';
     })
     // console.log(buffer);
     await Deno.writeTextFile("hello.txt", buffer);
